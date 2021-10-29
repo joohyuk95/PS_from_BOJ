83468389b620844c2b82bf90dd4e3b509f7b977d
joohyuk95 <jjang0306@naver.com>
2021-10-29 20:29:53 +0900

    1029 최단경로

--- shortest_distance/05.cpp
+++ shortest_distance/05.cpp
@@ -1,29 +1,56 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 #define INF 200000
 using namespace std;
+int dist[20001];
+vector<pair<int, int> > map[20001];
+
+class Info {
+public:
+    int vertex;
+    int weight;
+    Info(int a, int b) {
+        this->vertex = a;
+        this->weight = b;
+    }
+    bool operator<(const Info &rhs) const {
+        return this->weight > rhs.weight;
+    }
+};
+priority_queue <Info> pq;
 
 int main()
 {
     ios_base::sync_with_stdio(false);
     int v, e, k, a, b, c;
     cin >> v >> e >> k;
-    vector<vector<int> > map(v+1, vector<int> (v+1, INF));
     for (int i = 1; i <= e; ++i) {
         cin >> a >> b >> c;
-        if (map[a][b] > c) map[a][b] = c;
+        map[a].push_back(make_pair(b, c));
     }
-    for (int i = 1; i <= v; ++i) map[i][i] = 0;
-    for (int p = 1; p <= v; ++p) {
-        for (int i = 1; i <= v; ++i) {
-            for (int j = 1; j <= v; ++j) {
-                map[i][j] = min(map[i][j], map[i][p] + map[p][j]);
+    for (int i = 1; i <= v; ++i) dist[i] = INF;
+    dist[k] = 0;
+    pq.push(Info(k, 0));
+
+    while (!pq.empty()) {
+        int ver = pq.top().vertex;
+        int w = pq.top().weight;
+        pq.pop();
+        if (dist[ver] < w) continue;
+
+        for (int i = 0; i < map[ver].size(); ++i) {
+            int nextVertex = map[ver][i].first;
+            int nextWeight = map[ver][i].second;
+            if (dist[nextVertex] > dist[ver] + nextWeight) {
+                dist[nextVertex] = dist[ver] + nextWeight;
+                pq.push(Info(nextVertex, dist[nextVertex]));
             }
         }
     }
     for (int i = 1; i <= v; ++i) {
-        if (map[k][i] == INF) cout << "INF" << endl;
-        else cout << map[k][i] << endl;
-    }
+        if (dist[i] == INF) cout << "INF" << endl;
+        else cout << dist[i] << endl;
+    }   
     return 0;
 }
