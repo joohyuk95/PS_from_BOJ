c10fc8de6d411a5bf32c6001eea23c238059860c
joohyuk95 <jjang0306@naver.com>
2021-12-11 23:57:00 +0900

    1211 플로이드 2 푸는 중...

---
+++ shortest_distance/42.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <vector>
+#define INF 10000000
+using namespace std;
+
+int map[101][101];
+int cross[101][101];
+vector<int> path; 
+
+void Path_DFS(int start, int end)
+{
+    if (cross[start][end] == 0) {
+        path.push_back(start);
+        path.push_back(end);
+        return;
+    }
+    Path_DFS(start, cross[start][end]);
+    path.pop_back();
+    Path_DFS(cross[start][end], end);
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    for (int i = 1; i <= 100; ++i) {
+        for (int j = 1; j <= 100; ++j) {
+            if (i == j) continue;
+            else map[i][j] = INF;
+        }
+    }
+    
+    int n, m;
+    cin >> n >> m;
+    for (int i = 1; i <= m; ++i) {
+        int a, b, c; cin >> a >> b >> c;
+        map[a][b] = c;
+    }
+
+    for (int k = 1; k <= n; ++k) {
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                if (map[i][j] > map[i][k] + map[k][j]) {
+                    map[i][j] = map[i][k] + map[k][j];
+                    // cross[i][j] = k;
+                }
+            }
+        }
+    }
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= n; ++j) {
+            cout << map[i][j] << ' ';
+        }
+        cout << '\n';
+    }
+
+    // for (int i = 1; i <= n; ++i) {
+    //     for (int j = 1; j <= n; ++j) {
+    //         if (map[i][j] == INF || i == j) cout << 0;
+    //         else {
+    //             path.clear();
+    //             Path_DFS(i, j);
+    //             cout << path.size() << ' ';
+    //             for (int k = 0; k < path.size(); ++k) {
+    //                 cout << path[k] << ' ';
+    //             }
+    //         }
+    //         cout << '\n';
+    //     }        
+    // }
+
+    return 0;
+}
