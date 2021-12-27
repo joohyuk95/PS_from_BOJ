b2f6a5ed3b220fbb6baae7011d4aabaa67e40df2
joohyuk95 <jjang0306@naver.com>
2021-12-27 22:42:41 +0900

    1227 웜홈

--- shortest_distance/13.cpp
+++ shortest_distance/13.cpp
@@ -4,6 +4,9 @@ using namespace std;
 
 #define INF 5000000
 
+int tc, n, m, w;
+bool ncycle;
+
 int dist[501];
 
 class Edge {
@@ -18,40 +21,19 @@ public:
 };
 vector<Edge> Ed;
 
-bool bellmanFord(int n, int m, int w)
+bool bellmanFord()
 {
-    int a, b, c;
-    Ed.clear();
-    for (int i = 1; i <= n; ++i) dist[i] = INF;
-    for (int i = 1; i <= m; ++i) {
-        cin >> a >> b >> c;
-        Ed.push_back(Edge(a, b, c));
-        Ed.push_back(Edge(b, a, c));
-    }
-    for (int i = 1; i <= w; ++i) {
-        cin >> a >> b >> c;
-        Ed.push_back(Edge(a, b, -c));
-    }
-
     dist[1] = 0;
-    for (int i = 1; i <= n-1; ++i) {
+    for (int i = 1; i <= n; ++i) {
         for (int j = 0; j < Ed.size(); ++j) {
             int s = Ed[j].s;
             int e = Ed[j].e;
             int t = Ed[j].t;
             
-            if (dist[s] == INF) continue;
-            if (dist[e] > dist[s] + t) dist[e] = dist[s] + t;
-        }
-    }
-    for (int i = 0; i < Ed.size(); ++i) {
-        int s = Ed[i].s;
-        int e = Ed[i].e;
-        int t = Ed[i].t;
-        
-        if (dist[s] == INF) continue;
-        if (dist[e] > dist[s] + t) {
-            return true;
+            if (dist[e] > dist[s] + t) {
+                dist[e] = dist[s] + t;
+                if (i == n) return true;
+            }
         }
     }
     return false;
@@ -60,15 +42,24 @@ bool bellmanFord(int n, int m, int w)
 int main()
 {
     ios_base::sync_with_stdio(false);
-    int tc;
-    vector<bool> ncycle(5);
     cin >> tc;
-    for (int i = 0; i < tc; ++i) {
-        int a, b, c; cin >> a >> b >> c;
-        ncycle[i] = bellmanFord(a, b, c);
-    }
-    for (int i = 0; i < tc; ++i) {
-        if (ncycle[i]) cout << "YES" << endl;
+    for (int k = 1; k <= tc; ++k) {
+        Ed.clear();
+        for (int i = 1; i <= n; ++i) dist[i] = INF;
+
+        cin >> n >> m >> w;
+        for (int i = 1; i <= m; ++i) {
+            int a, b, c; cin >> a >> b >> c;
+            Ed.push_back(Edge(a, b, c));
+            Ed.push_back(Edge(b, a, c));
+        }
+        for (int i = 1; i <= w; ++i) {
+            int a, b, c; cin >> a >> b >> c;
+            Ed.push_back(Edge(a, b, -c));
+        }
+        
+        ncycle = bellmanFord();
+        if (ncycle) cout << "YES" << endl;
         else cout << "NO" << endl;
     }
     return 0;
