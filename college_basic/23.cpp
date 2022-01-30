c9506af22144f484a30f68a954ab26f51db392ed
joohyuk95 <jjang0306@naver.com>
2022-01-30 19:57:58 +0900

    0130 멀티탭 스케줄링

--- college_basic/23.cpp
+++ college_basic/23.cpp
@@ -1,15 +1,9 @@
-// k번 반복
-// 1. 비어있음
-// 2. 같은 행동
-// 3. 바꿔 꼽기
-
 #include <iostream>
-#include <vector>
 using namespace std;
 
-int n, k;
+int n, k, ans;
 int seq[100];
-int tab[100];
+int tap[100];
 
 int main()
 {
@@ -20,30 +14,45 @@ int main()
     }
     for (int i = 0; i < k; ++i) {
         bool plugin = false;
-        
-        for (int j = 0; j < k; ++j) {
-            if (!tab[j]) {
-                tab[j] = seq[i];
-                plugin = true;
-                break;
-            }
-        }
-        if (plugin) continue;
-
-        for (int j = 0; j < k; ++j) {
-            if (tab[j] == seq[i]) {
-                plugin = true;
-                break;
-            }
-        }
-        if (plugin) continue;
-
+        // 이미 같은 것이 꽂혀있는지 체크
         for (int j = 0; j < n; ++j) {
-            
+            if (tap[j] == seq[i]) {
+                plugin = true;
+                break;
+            }
         }
+        if (plugin) continue;
 
+        // 빈 공간이 있는지 체크
+        for (int j = 0; j < n; ++j) {
+            if (!tap[j]) {
+                tap[j] = seq[i];
+                plugin = true;
+                break;
+            }
+        }
+        if (plugin) continue;
+
+        // 하나를 선택해서 뽑아야 하는 상황
+        // 다시 안 쓰이거나 가장 나중에 쓰이는걸 뽑음
+        int plugout;   // 뺄 곳의 인덱스
+        int latest = 0;    // 가장 느린게 얼마나 느린지
+        for (int j = 0; j < n; ++j) {
+            int when = 1;
+            for (int p = i + 1; p < k; ++p) { // j구에 꽂힌 기기가 다음번에 몇 번째에 또 사용되나
+                //when++;     --> 여기 있으면 다시 안 쓰이는 거랑, 마지막 순서에 쓰이는 거를 구분할 수 없음
+                if (tap[j] == seq[p]) break;
+                when++;
+            }
+            if (when > latest) {
+                plugout = j;
+                latest = when;
+            }
+        }
+        ans++;
+        tap[plugout] = seq[i];
     }
 
-
+    cout << ans << '\n';
     return 0;
 }
