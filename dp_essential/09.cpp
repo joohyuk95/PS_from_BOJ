fb3e80acf699d37b9b08f61d5d6ac56911b6daa3
joohyuk95 <jjang0306@naver.com>
2022-02-24 21:27:42 +0900

    0224 이친수

---
+++ dp_essential/09.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int n;
+long long dp[91];
+
+int main()
+{
+    cin >> n;
+
+    dp[1] = 1;
+    dp[2] = 1;
+
+    for (int i = 3; i <= n; ++i) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+
+    cout << dp[n] << '\n';
+    return 0;
+}
