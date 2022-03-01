1451a655c07132dce9f025f8cdb497128874ecde
joohyuk95 <jjang0306@naver.com>
2022-03-01 22:00:00 +0900

    0301 동전 2 푸는 중...

---
+++ dp_essential/12.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n, k;
+int value[101];
+int dp[10001];
+
+int main() 
+{
+    ios_base::sync_with_stdio(false);
+    cin >> n >> k;
+
+    for (int i = 1; i <= n; ++i) {
+        cin >> value[i];
+    }
+    sort(value, value + n);
+
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= k; ++j) {
+            ;
+        }
+    }
+
+    if (true) {
+        ;
+    } else {
+        ;
+    }
+
+    return 0;
+}
