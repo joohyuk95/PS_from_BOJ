2e3fa3ec309c2bbde35039e6374bf85bfa81286a
joohyuk95 <jjang0306@naver.com>
2022-03-02 19:35:02 +0900

    0302 동전 2

--- dp_essential/12.cpp
+++ dp_essential/12.cpp
@@ -1,32 +1,28 @@
 #include <iostream>
-#include <algorithm>
 using namespace std;
 
 int n, k;
 int value[101];
-int dp[10001];
+int dp[100001];
 
 int main() 
 {
     ios_base::sync_with_stdio(false);
     cin >> n >> k;
-
+    for (int i = 1; i <= k; ++i) dp[i] = 100001;
     for (int i = 1; i <= n; ++i) {
         cin >> value[i];
+        dp[value[i]] = 1;
     }
-    sort(value, value + n);
-
+     
     for (int i = 1; i <= n; ++i) {
-        for (int j = 1; j <= k; ++j) {
-            ;
+        for (int j = value[i]; j <= k; ++j) {    
+            dp[j] = min(dp[j], dp[j - value[i]] + 1);
         }
     }
 
-    if (true) {
-        ;
-    } else {
-        ;
-    }
+    if (dp[k] == 100001) cout << -1 << '\n';
+    else cout << dp[k] << '\n';
 
     return 0;
 }
