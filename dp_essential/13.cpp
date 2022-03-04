809ea2a0bea3d82ffb58e31e9a14c02a71be3e48
joohyuk95 <jjang0306@naver.com>
2022-03-04 16:53:36 +0900

    0304 카드 구매하기

--- dp_essential/13.cpp
+++ dp_essential/13.cpp
@@ -10,13 +10,10 @@ int main()
     ios_base::sync_with_stdio(false);
     cin >> n;
     for (int i = 1; i <= n; ++i) cin >> pack[i];
-    // dp[i] : i개의 카드를 구입하기 위한 비용의 최댓값.
 
     for (int i = 1; i <= n; ++i) {
-        for (int j = 1; j <= n; ++j) {
-            int prev = j - pack[i];
-            if (prev < 0) continue;
-            dp[j] = min(dp[j], pack[prev] + pack[i]);
+        for (int j = 1; j <= i; ++j) {
+            dp[i] = max(dp[i], dp[i - j] + pack[j]);
         }
     }
 
