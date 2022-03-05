5b9e2f3de617cd8c342a17e18ca8e848ddeb1048
joohyuk95 <jjang0306@naver.com>
2022-03-05 19:01:10 +0900

    0305 제곱수의 합

---
+++ dp_essential/14.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int n;
+int dp[100001];
+
+int main()
+{
+    cin >> n;
+    for (int i = 1; i <= n; ++i) {
+        dp[i] = i;
+        for (int j = 1; j * j <= i; ++j) {
+            dp[i] = min(dp[i], dp[i - j * j] + 1);
+        }
+    }
+    
+    cout << dp[n] << '\n';
+    return 0;
+}
