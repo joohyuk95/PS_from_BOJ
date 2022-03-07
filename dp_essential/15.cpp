dcaabc28fed595ddc9e11d5a35da801513a2da82
joohyuk95 <jjang0306@naver.com>
2022-03-07 23:30:22 +0900

    0307 1,2,3 더하기

--- dp_essential/15.cpp
+++ dp_essential/15.cpp
@@ -2,23 +2,23 @@
 using namespace std;
 
 int t, n;
-int dp[11];
+int dp[12];
 
 int main()
 {
     ios_base::sync_with_stdio(false);
-    cin >> t;
+    
     dp[1] = 1;
     dp[2] = 2;
     dp[3] = 4;
-
-    for (int i = 1; i <= t; ++i) {
-        cin >> n;
-        for (int j = 4; j <= n; ++j) {
-            dp[j] = dp[j - 1] + dp[j - 2] + dp[j - 3];
-        }
-        cin >> dp[n];
+    for (int i = 4; i < 11; ++i) {
+        dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
     }
 
+    cin >> t;
+    for (int i = 1; i <= t; ++i) {
+        cin >> n;
+        cout << dp[n] << '\n';
+    }
     return 0;
 }
