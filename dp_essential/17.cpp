914c31f468c299155b03b3bfdfd7de6ff0dbdc21
joohyuk95 <jjang0306@naver.com>
2022-03-10 23:30:03 +0900

    0310 합분해

--- dp_essential/17.cpp
+++ dp_essential/17.cpp
@@ -2,22 +2,20 @@
 #define MOD 1000000000
 using namespace std;
 
-int n, k, res;
+int n, k;
 long long dp[201][201];
 
 int main()
 {
     cin >> n >> k;
-    for (int i = 1; i <= n; ++i) {
-        dp[0][i] = 1;
+    for (int i = 0; i <= n; ++i) {
         dp[1][i] = 1;
     }
     
-
     for (int i = 2; i <= k; ++i) {
-        for (int j = 1; j <= n; ++j) {
-            for (int p = 0; p <= n; ++p) {
-                dp[i][j] += dp[i - 1][j];
+        for (int j = 0; j <= n; ++j) {
+            for (int p = 0; p <= j; ++p) {
+                dp[i][j] += dp[i - 1][p];
             }
             dp[i][j] %= MOD;
         }
