bb1510b38ad3ef5a6d3ed6ef737b4f7152c8a8af
joohyuk95 <jjang0306@naver.com>
2022-06-25 22:00:00 +0900

    0625 다음 순열

---
+++ coding_test/07.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n;
+int nums[10001];
+bool flag;
+
+int main()
+{
+    cin >> n;
+    if (n == 1) {
+        cout << -1 << '\n';
+        return 0;
+    }
+    for (int i = 1; i <= n; ++i) cin >> nums[i];
+
+    int i;
+    for (i = n-1; i >= 1; --i) {
+        if (nums[i] > nums[i + 1]) {
+            if (i == 1) flag = true;
+            continue;
+        } else break;
+    }
+    if (flag) {
+        cout << -1 << '\n';
+        return 0;
+    }
+
+    int ref = nums[i];
+    int mini = 20000;
+    int idx;
+    for (int j = n; j > i; --j) {
+        if (nums[j] > ref && nums[j] <= mini) {
+            mini = nums[j];
+            idx = j;
+        }
+    }
+    
+    nums[i] = nums[idx];
+    nums[idx] = ref;
+    sort(nums+i+1, nums + n+1);
+    
+    for (int i = 1; i <= n; ++i) cout << nums[i] << ' ';
+    return 0;
+}
